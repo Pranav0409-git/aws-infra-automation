3d8009748154ac48609641fb5a9b0fca54f678b5
EC2 Default User <ec2-user@ip-172-31-83-24.ec2.internal>
2026-02-12 11:20:20 +0000

    Added Terraform AWS infra code

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "var.vpc_cidr"
+    tags = {
+      name = "Test-vpc"
+    }
+  
+}
+resource "aws_subnet" "public" {
+    count = 2
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = var.public_subnets[count.index]
+    availability_zone = var.azs[count.index]
+    map_public_ip_on_launch = true
+
+    tags = {
+      name = "public_subnet-${count.index + 1}"
+    }
+}
+resource "aws_subnet" "private" {
+    count = 2
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = var.private_subnets[count.index]
+    availability_zone = var.azs[count.index]
+
+    tags = {
+      name = "private_subnet-${count.index + 1}"
+    }
+  
+}
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.my_vpc.id
+  
+}
